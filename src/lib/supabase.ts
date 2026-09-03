462d473d85e04971cf93082ec8b4ed754b6fa82c
cbmacademydelhi-source <cbmacademydelhi@gmail.com>
2026-09-03 03:57:34 -0700

    Create supabase.ts

---
+++ src/lib/supabase.ts
@@ -0,0 +1,12 @@
+import { createClient } from "@supabase/supabase-js";
+
+const SUPABASE_URL =
+  "https://cxeztsvgaeytdljoned.supabase.co";
+
+const SUPABASE_PUBLISHABLE_KEY =
+  "sb_publishable_sO0rnN9MJzQmBiq-Lm6p3w_EAYKjVOb";
+
+export const supabase = createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
+);
